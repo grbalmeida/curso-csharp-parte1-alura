ab6902d4ec7c02d028b578f1425309e53c9ceb0d
gilvan <g.r.almeida@live.com>
2020-08-13 23:05:57 -0300

    Conversão e outros tipos numéricos

---
+++ AprendendoCSharp/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario = 1200.50;
+
+            // O int é um tipo de variável de até 32 bits
+            int salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            // O long é um tipo de variável de 64 bits
+            long idade = 13_000_000_000;
+            Console.WriteLine(idade);
+
+            // O short é um tipo de variável de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
